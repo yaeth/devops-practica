f6aac32a30806c290cc7603de1f24974b9860be7
yaeth <yaeth.ochoa@gmail.com>
2025-08-27 17:15:19 -0400

    q

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hola, mundo desde Docker!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.router("/")
+def home():
+    return "Hola, Mundo desde docker con Flask!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+        
