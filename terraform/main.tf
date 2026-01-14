d884c9b5b5699081f0403652501df78a301f652c
yaeth <83660751+yaeth@users.noreply.github.com>
2026-01-14 10:31:55 -0300

    Add Azure Kubernetes Service configuration

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group
+  dns_prefix          = "aksdemo"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
